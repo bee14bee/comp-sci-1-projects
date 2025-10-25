64c89987a4f1dae287d524a5d772882c7079fa71
bee14bee <brit.rod1005@gmail.com>
2025-10-25 14:54:56 -0500

    Add findMax and sumArray functions
    Implement functions to find the maximum value and sum of an array.

---
+++ funcs n 2D arrs.cpp
@@ -0,0 +1,26 @@
+/* 
+Write a function int findMax(int arr[], int size) that returns the largest value.
+
+Write int sumArray(int arr[], int size) that returns the total sum.
+*/
+
+#include <iostream>
+using namespace std;
+
+// 1
+int findMax(int arr[i], int size)
+{
+  int size = 5;
+  for (int i = 0; i < size; i++)
+  {
+    cout<<"Enter numbers";
+    return arr[i];
+  }
+}
+
+int main(){
+
+// 1
+  
+  cout<<findMax(size,5)
+
